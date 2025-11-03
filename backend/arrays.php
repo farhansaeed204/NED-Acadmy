376d15badc76d9f2314001f1b3f5643127a0dcf9
Farhan_Saeed <128322226+farhansaeed204@users.noreply.github.com>
2025-11-03 13:36:43 +0500

    Add files via upload

---
+++ backend/arrays.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $arr = array("frahan","saad","maaz");
+    // array are print with help of echo but only indexing work not full array
+    echo $arr[1]. "<br>";
+    // print full array in one line
+    print_r($arr);
+
+    ?>
+    
+</body>
+</html>
---
+++ backend/functions.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // functions in php
+    function Name ($name){
+        echo $name. "<br>";
+    };
+
+    function add ($a, $b){
+       return $a + $b; 
+    };
+
+    Name("Farhan");
+
+    echo add(5,6)
+    ?>
+</body>
+</html>
