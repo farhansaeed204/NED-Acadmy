107f98f42a0be6f278287ef388924d2e607605fa
Farhan_Saeed <128322226+farhansaeed204@users.noreply.github.com>
2025-11-05 14:50:29 +0500

    Add files via upload

---
+++ HTML_Forms_to_PHP/form_validation.php
@@ -0,0 +1,19 @@
+<?php
+// empty string check
+
+// this all value is empty means ans true
+// $name = "";
+// $name = 0;
+// $name = "0";
+// $name = null;
+// $name = false;
+// $name = [];
+// echo empty($name);       // answer is 1 its mean true in php or ans is empty its mean false
+
+// check email format 
+// $email = "farhansaeed@gmail.com";
+// echo filter_var($email, FILTER_VALIDATE_EMAIL);  // ans return email means email format is correct or ans is empty means format is worng
+
+// HTML special characters
+echo $_POST["username"]
+?>
